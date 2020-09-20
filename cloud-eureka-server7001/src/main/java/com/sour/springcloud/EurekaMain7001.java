bef46a474d9bf99e6c68d4478ae09c607cfc1d93
Sourlun <1141837289@qq.com>
2020-09-20 18:06:37 +0800

    新建 注册中心 eureka-server7001

---
+++ cloud-eureka-server7001/src/main/java/com/sour/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.sour.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author xgl
+ * @date 2020/9/20 18:04
+ **/
+@SpringBootApplication
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
