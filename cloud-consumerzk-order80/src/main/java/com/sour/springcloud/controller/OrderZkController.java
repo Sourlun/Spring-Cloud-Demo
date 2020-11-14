c401a8eed8fa0a50da3c5ec2825dbc4db2f741b7
Sourlun <1141837289@qq.com>
2020-11-14 13:21:21 +0800

    添加zookeeper消费者80工程: cloud-consumer-order80

---
+++ cloud-consumerzk-order80/src/main/java/com/sour/springcloud/controller/OrderZkController.java
@@ -0,0 +1,29 @@
+package com.sour.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author xgl
+ * @date 2020/11/14 11:25
+ **/
+@RestController
+@Slf4j
+public class OrderZkController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject( INVOKE_URL + "/payment/zk", String.class);
+        return result;
+    }
+
+}
